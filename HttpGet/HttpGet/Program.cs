810e15eacae50685e9410683c60d1ea72c4adee8
Ted Johnson <johnson520@gmail.com>
2016-04-13 22:20:49 -0700

    Made it work!

--- HttpGet/HttpGet/Program.cs
+++ HttpGet/HttpGet/Program.cs
@@ -1,72 +1,12 @@
 ﻿using System;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HttpGet
 {
-    internal class Program
+    internal static class Program
     {
-        private const string GetHeader = @"GET / HTTP/1.1
-Host: {hostname}
-Connection: close
-User-Agent: MyHttpGetter/1.0 (+http://www.baccano.com/)
-Accept-Charset: ISO-8859-1,UTF-8;q=0.7,*;q=0.7
-Cache-Control: no-cache
-Accept-Language: en-us
-Referer: http://www.baccano.com/
-";
-
-        private static async Task<string> HttpRequestAsync(string hostName)
-        {
-            using (var tcp = new TcpClient(hostName, 80))
-            {
-                using (var stream = tcp.GetStream())
-                {
-                    tcp.SendTimeout = 500;
-                    tcp.ReceiveTimeout = 1000;
-                    // Send request headers
-                    var builder = new StringBuilder();
-                    builder.AppendLine("GET /?scope=images&nr=1 HTTP/1.1");
-                    builder.AppendLine($"Host: {hostName}");
-                    //builder.AppendLine("Content-Length: " + data.Length);   // only for POST request
-                    builder.AppendLine("Connection: close");
-                    builder.AppendLine();
-                    var header = Encoding.ASCII.GetBytes(builder.ToString());
-                    await stream.WriteAsync(header, 0, header.Length);
-
-                    // Send payload data if you are POST request
-                    //await stream.WriteAsync(data, 0, data.Length);
-
-                    // receive data
-                    using (var memory = new MemoryStream())
-                    {
-                        await stream.CopyToAsync(memory);
-                        memory.Position = 0;
-                        var data = memory.ToArray();
-
-                        var crlf = Encoding.ASCII.GetBytes("\r\n");
-                        int i;
-                        for (i = 0; i < data.Length - 4; ++i)
-                        {
-                            if (data[i] == crlf[0] && data[i + 1] == crlf[1] && data[i + 2] == crlf[0] && data[i + 3] == crlf[1])
-                            {
-                                i += 4;
-                                break;
-                            }
-                        }
-
-                        var responseHeaders = Encoding.ASCII.GetString(data, 0, i);
-                        Console.WriteLine("Response headers: " + responseHeaders);
-                        memory.Position = i;
-                        return Encoding.UTF8.GetString(data, i, data.Length - i);
-                    }
-                }
-            }
-        }
-
         private static int Main(string[] args)
         {
             if (args.Length != 1)
@@ -75,47 +15,62 @@ Referer: http://www.baccano.com/
                 return 1;
             }
 
-            var response = HttpRequestAsync(args[0]).Result;
-            Console.WriteLine("Response body: " + response);
+            var uri = new Uri(args[0]);
 
-            Console.WriteLine("Press enter to continue...");
-            Console.Read();
-            return 0;
-
-            var hostEntry = Dns.GetHostEntry(args[0]);
+            var hostEntry = Dns.GetHostEntry(uri.Host);
             if (hostEntry.AddressList.Length > 0)
             {
-                var client = new TcpClient(hostEntry.HostName, 80) { NoDelay = true };
+                using (var client = new TcpClient())
+                {
+                    client.NoDelay = true;
+                    client.Connect(hostEntry.AddressList[0], 80);
 
-                // Translate the passed message into ASCII and store it as a Byte array.
-                var data = Encoding.ASCII.GetBytes(GetHeader);
+                    const string CRLF = "\r\n";
+                    const string CRLFx2 = CRLF + CRLF;
+                    var header = "GET " + uri.LocalPath + " HTTP/1.1" + CRLF
+                                 + "Host: " + uri.Host + CRLF
+                                 + "Connection: close" + CRLFx2;
 
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
+                    var headerBytes = Encoding.ASCII.GetBytes(header);
 
-                var stream = client.GetStream();
+                    using (var stream = client.GetStream())
+                    {
+                        stream.Write(headerBytes, 0, headerBytes.Length);
 
-                // Send the message to the connected TcpServer. 
-                stream.Write(data, 0, data.Length);
+                        var sb = new StringBuilder();
+                        int count;
+                        do
+                        {
+                            var buffer = new byte[1024];
+                            count = stream.Read(buffer, 0, buffer.Length);
+                            sb.Append(Encoding.ASCII.GetString(buffer, 0, count));
+                        } while (count > 0);
 
-                Console.WriteLine("Sent get header");
+                        var fullResponse = sb.ToString();
+                        var endOfHeaderIndex = fullResponse.IndexOf(CRLFx2);
+                        if (endOfHeaderIndex != -1)
+                        {
+                            Console.WriteLine("Response header:\n\n" + fullResponse.Substring(0, endOfHeaderIndex));
 
-                // Receive the TcpServer.response.
+                            Console.WriteLine("\n\nPress a key to continue...");
+                            Console.ReadKey();
 
-                // Buffer to store the response bytes.
-                data = new byte[256];
+                            Console.WriteLine("Response body:\n\n" + fullResponse.Substring(endOfHeaderIndex + CRLF.Length));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Received: {0}", fullResponse);
+                        }
 
-                // String to store the response ASCII representation.
+                        stream.Close();
+                    }
 
-                // Read the first batch of the TcpServer response bytes.
-                var bytes = stream.Read(data, 0, data.Length);
-                var responseData = Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
-
-                // Close everything.
-                stream.Close();
-                client.Close();
+                    client.Close();
+                }
             }
+
+            Console.WriteLine("\n\nPress a key to continue...");
+            Console.ReadKey();
 
             return 0;
         }
